e8e123108bebbfb0aaf71c96e53c93cb51e629fa
Simon Binder <simolus3@gmail.com>
2019-04-06 17:47:24 +0200

    RLP implementation that doesn't depend on dart:io

--- lib/src/utils/rlp.dart
+++ lib/src/utils/rlp.dart
@@ -1,11 +1,41 @@
-import 'dart:io';
+import 'dart:convert';
 import 'dart:typed_data';
 
+import 'package:meta/meta.dart';
+import 'package:typed_data/typed_buffers.dart';
 import 'package:web3dart/crypto.dart';
 
-// todo don't depend on dart:io
+@visibleForTesting
+class LengthTrackingByteSink extends ByteConversionSinkBase {
 
-void encodeString(Uint8List string, BytesBuilder builder) {
+  final Uint8Buffer _buffer = Uint8Buffer();
+  int _length = 0;
+
+  int get length => _length;
+
+  Uint8List asBytes() {
+    return _buffer.buffer.asUint8List(0, _length);
+  }
+
+  @override
+  void add(List<int> chunk) {
+    _buffer.addAll(chunk);
+    _length += chunk.length;
+  }
+
+  void addByte(int byte) {
+    _buffer.add(byte);
+    _length++;
+  }
+
+  @override
+  void close() {
+    // no-op, never used
+  }
+
+}
+
+void encodeString(Uint8List string, LengthTrackingByteSink builder) {
   // For a single byte in [0x00, 0x7f], that byte is its own RLP encoding
   if (string.length == 1 && string[0] <= 0x7f) {
     builder.addByte(string[0]);
@@ -32,8 +62,8 @@ void encodeString(Uint8List string, BytesBuilder builder) {
     ..add(string);
 }
 
-void encodeList(List list, BytesBuilder builder) {
-  final subBuilder = BytesBuilder();
+void encodeList(List list, LengthTrackingByteSink builder) {
+  final subBuilder = LengthTrackingByteSink();
   for (var item in list) {
     _encodeToBuffer(item, subBuilder);
   }
@@ -42,7 +72,7 @@ void encodeList(List list, BytesBuilder builder) {
   if (length <= 55) {
     builder
       ..addByte(0xc0 + length)
-      ..add(subBuilder.takeBytes());
+      ..add(subBuilder.asBytes());
     return;
   } else {
     final encodedLength = intToBytes(BigInt.from(length));
@@ -50,12 +80,12 @@ void encodeList(List list, BytesBuilder builder) {
     builder
       ..addByte(0xf7 + encodedLength.length)
       ..add(encodedLength)
-      ..add(subBuilder.takeBytes());
+      ..add(subBuilder.asBytes());
     return;
   }
 }
 
-void _encodeInt(BigInt val, BytesBuilder builder) {
+void _encodeInt(BigInt val, LengthTrackingByteSink builder) {
   if (val == BigInt.zero) {
     encodeString(Uint8List(0), builder);
   } else {
@@ -63,7 +93,7 @@ void _encodeInt(BigInt val, BytesBuilder builder) {
   }
 }
 
-void _encodeToBuffer(dynamic value, BytesBuilder builder) {
+void _encodeToBuffer(dynamic value, LengthTrackingByteSink builder) {
   if (value is Uint8List) {
     encodeString(value, builder);
   } else if (value is List) {
@@ -78,7 +108,8 @@ void _encodeToBuffer(dynamic value, BytesBuilder builder) {
 }
 
 List<int> encode(dynamic value) {
-  final builder = BytesBuilder();
+  final builder = LengthTrackingByteSink();
   _encodeToBuffer(value, builder);
-  return builder.takeBytes();
+
+  return builder.asBytes();
 }
--- test/utils/rlp_test.dart
+++ test/utils/rlp_test.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:web3dart/src/utils/rlp.dart';
@@ -7,24 +6,24 @@ import 'package:test_api/test_api.dart';
 
 void main() {
   test('encodes short strings', () {
-    final builder = BytesBuilder();
+    final builder = LengthTrackingByteSink();
     encodeString(ascii.encode('dog'), builder);
 
-    expect(builder.toBytes(), [0x83].followedBy(ascii.encode('dog')));
+    expect(builder.asBytes(), [0x83].followedBy(ascii.encode('dog')));
   });
 
   test('encodes long strings', () {
-    final builder = BytesBuilder();
+    final builder = LengthTrackingByteSink();
     final payload = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit';
 
     encodeString(ascii.encode(payload), builder);
 
-    expect(builder.toBytes(), [0xb8, 0x38].followedBy(ascii.encode(payload)));
+    expect(builder.asBytes(), [0xb8, 0x38].followedBy(ascii.encode(payload)));
   });
 
   test('encodes empty string', () {
-    final builder = BytesBuilder();
+    final builder = LengthTrackingByteSink();
     encodeString(Uint8List(0), builder);
-    expect(builder.takeBytes(), [0x80]);
+    expect(builder.asBytes(), [0x80]);
   });
 }
