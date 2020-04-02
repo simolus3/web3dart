8a1dd1f713c74e2acae579a29d7dd562073b2fca
Jordan Krage <jmank88@gmail.com>
2020-04-02 08:19:54 -0500

    support eth_getLogs (#86)
    Add getLogs method to Web3Client

--- lib/src/core/client.dart
+++ lib/src/core/client.dart
@@ -252,6 +252,19 @@ class Web3Client {
         'eth_getCode', [address.hex, _getBlockParam(atBlock)]).then(hexToBytes);
   }
 
+  /// Returns all logs matched by the filter in [options].
+  ///
+  /// See also:
+  ///  - [events], which can be used to obtain a stream of log events
+  ///  - https://github.com/ethereum/wiki/wiki/JSON-RPC#eth_getlogs
+  Future<List<FilterEvent>> getLogs(FilterOptions options) {
+    final filter = _EventFilter(options);
+    return _makeRPCCall<List<dynamic>>(
+        'eth_getLogs', [filter._createParamsObject(true)]).then((logs) {
+      return logs?.map(filter.parseChanges)?.toList();
+    });
+  }
+
   /// Signs the given transaction using the keys supplied in the [cred]
   /// object to upload it to the client so that it can be executed.
   ///
