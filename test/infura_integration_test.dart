2d4e2f85e3a76f6152a83444aed8bd01741c9b40
Simon Binder <oss@simonbinder.eu>
2021-05-22 23:01:03 +0200

    Add integration test for erc20

---
+++ test/infura_integration_test.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+
+import 'package:http/http.dart';
+import 'package:test/test.dart';
+import 'package:web3dart/contracts/erc20.dart';
+import 'package:web3dart/web3dart.dart';
+
+void main() {
+  final infuraProjectId = Platform.environment['INFURA_ID'];
+
+  group(
+    'integration',
+    () {
+      late final Web3Client client;
+
+      setUpAll(() {
+        client = Web3Client(
+            'https://mainnet.infura.io/v3/$infuraProjectId', Client());
+      });
+
+      // ignore: unnecessary_lambdas, https://github.com/dart-lang/linter/issues/2670
+      tearDownAll(() => client.dispose());
+
+      test('erc20 get balance', () async {
+        final shibaInu = Erc20(
+          address: EthereumAddress.fromHex(
+              '0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce'),
+          client: client,
+        );
+
+        final balance = await shibaInu.balanceOf(EthereumAddress.fromHex(
+            '0xdead000000000000000042069420694206942069'));
+
+        expect(balance >= BigInt.parse('410243042034234643784017156276017'),
+            isTrue);
+      });
+    },
+    skip: infuraProjectId == null
+        ? 'Tests require the INFURA_ID environment variable'
+        : null,
+  );
+}
