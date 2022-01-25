96a66a3a9bd758eff4bf104fe28db19dbc7c60d3
Alexey Inkin <leha@inkin.ru>
2022-01-25 12:11:27 +0300

    Issue #210 Add timestamp to BlockInformation

--- lib/src/core/block_information.dart
+++ lib/src/core/block_information.dart
@@ -2,16 +2,22 @@ import 'package:web3dart/src/crypto/formatting.dart';
 import 'package:web3dart/web3dart.dart';
 
 class BlockInformation {
-  EtherAmount? baseFeePerGas;
+  final EtherAmount? baseFeePerGas;
+  final int timestamp;
 
-  BlockInformation({this.baseFeePerGas});
+  BlockInformation({
+    required this.baseFeePerGas,
+    required this.timestamp,
+  });
 
   factory BlockInformation.fromJson(Map<String, dynamic> json) {
     return BlockInformation(
-        baseFeePerGas: json.containsKey('baseFeePerGas')
-            ? EtherAmount.fromUnitAndValue(
-                EtherUnit.wei, hexToInt(json['baseFeePerGas'] as String))
-            : null);
+      baseFeePerGas: json.containsKey('baseFeePerGas')
+          ? EtherAmount.fromUnitAndValue(
+              EtherUnit.wei, hexToInt(json['baseFeePerGas'] as String))
+          : null,
+      timestamp: hexToDartInt(json['timestamp'] as String),
+    );
   }
 
   bool get isSupportEIP1559 => baseFeePerGas != null;
